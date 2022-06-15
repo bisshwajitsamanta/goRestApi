57e8ce95452e56ba1aceeee0e092d4e4e3a5869b
Bisshwajit Samanta <bishwajitsamanta1689@gmail.com>
2022-06-15 12:12:25 +0530

    Updated the Comments

--- internal/comment/comment.go
+++ internal/comment/comment.go
@@ -2,7 +2,13 @@ package comment
 
 import (
 	"context"
+	"errors"
 	"fmt"
+)
+
+var (
+	ErrFetchingComment = errors.New("Could not fetch the comment")
+	ErrNotImplemented  = errors.New("Not Yet Implemented")
 )
 
 // Comment - A representation of the comment structure of our service
@@ -22,7 +28,7 @@ type Service struct {
 	Store Store
 }
 
-//NewService - Structure Function
+//NewService - Structure Function ways to initiate constructor in Golang
 func NewService(store Store) *Service {
 	return &Service{
 		store,
@@ -39,4 +45,19 @@ func (s *Service) GetComment(ctx context.Context, id string) (Comment, error) {
 	}
 
 	return cmt, nil
+}
+
+// UpdateComment - It updates an existing comment
+func (s *Service) UpdateComment(ctx context.Context, comment Comment) error {
+	return ErrNotImplemented
+}
+
+// DeleteComment - It deletes an existing comment
+func (s *Service) DeleteComment(ctx context.Context, id string) error {
+	return ErrNotImplemented
+}
+
+// CreateComment - It creates an new comment
+func (s *Service) CreateComment(ctx context.Context, comment Comment) (Comment, error) {
+	return Comment{}, ErrNotImplemented
 }
